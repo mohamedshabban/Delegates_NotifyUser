d55eb4eee4369d8adbddca86e3eba9d7a895a983
Mohamed Shaban <57798592+mohamedshabban@users.noreply.github.com>
2024-10-29 12:48:08 +0300

    Init

---
+++ Delegates/Process.cs
@@ -0,0 +1,16 @@
+﻿namespace Delegates;
+
+public class Process
+{
+    public Notify? OnCompleted;
+    public void StartProcess()
+    {
+        Console.WriteLine("Process Started!!");
+        Thread.Sleep(1000);
+        OnCompleted?.Invoke();
+    }
+    public void NotifyUser()
+    {
+        Console.WriteLine("Process Completed!!");
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Delegates
+{
+
+    public delegate void Notify();
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Process process = new Process();
+            process.OnCompleted = process.NotifyUser;
+            process.StartProcess();
+        }
+    }
+}
+
+
+
